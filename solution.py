b601057c0e2643828b665ee2ee96184fb9e21e97
devou <idevou@gmail.com>
2012-12-12 16:19:20 +0100

    refactored

--- solution.py
+++ solution.py
@@ -1,12 +1,10 @@
 #!/vol/fob-vol3/mi12/adleralq/gentoo/usr/bin/python
 # encoding: utf-8
 
-from pprint import pprint
-from collections import defaultdict
+import sys
 
 from helpers import stripClassifications
 from features import Features
-import sys
 
 
 # reads an IOB file and „yield“s every sentence as a list of
@@ -31,6 +29,7 @@ def getUniqueTokens(filename):
             line.strip() for line in f.xreadlines()
         ]))
 
+
 # Returns a list of (featureSet, label) pairs from file f using
 # featureExtractor to find the featureSet. If verbose, then we
 # print a progress bar.
@@ -38,10 +37,10 @@ def readForTraining(f, featureExtractor, verbose=False):
     rv = []
     i = 0
     for sentence in readSentences(f):
-# sentence contains classifications which the feature extractor
-# shouldn't see
+        # sentence contains classifications which the feature extractor
+        # shouldn't see
         featureSet = featureExtractor.featuresForSentence(stripClassifications(sentence))
-# but the result should contain them as second element
+        # but the result should contain them as second element
         pair = zip(featureSet, map(lambda x: x[1], sentence))
         rv += pair
         i += 1
@@ -52,6 +51,7 @@ def readForTraining(f, featureExtractor, verbose=False):
             else: sys.stdout.write('.')
             sys.stdout.flush()
     return rv
+
 
 def solution():
     stopwords = getUniqueTokens("english_stop_words.txt")
