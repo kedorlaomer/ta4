20b40a294f63f8db847c45f0f198604330208899
devou <idevou@gmail.com>
2012-12-21 09:30:30 +0100

    added cross-validation

--- solution.py
+++ solution.py
@@ -1,7 +1,8 @@
 # encoding: utf-8
 
 import sys
-import random
+from random import random, shuffle
+
 import cPickle
 from os.path import isfile as os_isfile
 
@@ -9,6 +10,39 @@ from nltk import NaiveBayesClassifier
 from nltk import classify as nltk_classify
 
 from features import Features
+
+
+# reads an IOB file and „yield“s every sentence as a list of
+# pairs (token, classification)
+def readSentences(f):
+    accu = []
+    for l in f.xreadlines():
+        line = l.strip()
+        contents = line.split("\t")
+        if len(contents) != 2:
+            if len(accu) > 0:
+                yield accu
+                accu = []
+        else:
+            token, classification = contents
+            accu.append((token, classification))
+
+
+# „yields“ n pairs of (training, test) where ⅕ of the data are
+# randomly chosen to be test-Data
+def makeCrossValidationFiles(f, parts=5):
+    sentences = list(readSentences(f))
+    shuffle(sentences)
+
+    part_count = len(sentences) / parts
+    for i in xrange(parts):
+        if i == parts - 1:
+            test = sentences[i * part_count:]
+        else:
+            test = sentences[i * part_count:(i + 1) * part_count]
+
+        train = sentences[:i * part_count] + sentences[(i + 1) * part_count:]
+        yield (train, test)
 
 
 def iterFileTokens(filename):
@@ -39,6 +73,25 @@ def saveOutput(filename, classifiedTokens):
     print "Classified output was saved to file '%s'" % filename
 
 
+def writeCorpus(l):
+    # parts?!
+    for i, parts in enumerate(l):
+        trainF = open('train%s' % i, 'w')
+        testF = open('test%s' % i, 'w')
+
+        for line in parts[0]:
+            for pair in line:
+                trainF.write("%s\t%s\n" % pair)
+            trainF.write('\n')
+        for line in parts[1]:
+            for pair in line:
+                testF.write("%s\t%s\n" % pair)
+            testF.write('\n')
+
+        trainF.close()
+        testF.close()
+
+
 def dumpClassifier(filename, classifier):
     with open(filename, 'wb') as f:
         cPickle.dump(classifier, f)
@@ -65,11 +118,8 @@ def train(trainSetFilename, classifierFilename='classifier.classi'):
     featuresets = []
 
     prevClassification = ''
-    tokenCount = 0
+    # tokenCount = 0
     for token, classification in classifiedTokens:
-
-        classification = (classification != "O")
-
         currFeatures = features.featuresForWord(token)
         if prevClassification:
             currFeatures['prevClassification'] = prevClassification
@@ -79,15 +129,15 @@ def train(trainSetFilename, classifierFilename='classifier.classi'):
             currFeatures, classification,
         ))
 
-        if tokenCount % 10000 == 0:
-            print "%s/%s" % (tokenCount, len(classifiedTokens))
-        tokenCount += 1
+        # if tokenCount % 10000 == 0:
+        #     print "%s/%s" % (tokenCount, len(classifiedTokens))
+        # tokenCount += 1
 
     # train_set = []
     # test_set = []
     # coeff = 2 / 3.0
     # for i in xrange(tokenCount):
-    #     if random.random() > coeff:
+    #     if random() > coeff:
     #         test_set.append(featuresets[i])
     #     else:
     #         train_set.append(featuresets[i])
@@ -95,7 +145,7 @@ def train(trainSetFilename, classifierFilename='classifier.classi'):
     # print len(test_set)
 
     classifier = NaiveBayesClassifier.train(featuresets)
-    # print classifier.show_most_informative_features(20)
+    # print classifier.show_most_informative_features(10)
     # print nltk_classify.accuracy(classifier, test_set)
     dumpClassifier(classifierFilename, classifier)
 
@@ -131,12 +181,20 @@ def classify(
     saveOutput(predictFilename, classifiedTokens)
 
 
+def makeCrossValidation(train_set_filename):
+    with open(train_set_filename) as f:
+        groupped_sentences = makeCrossValidationFiles(f)
+        writeCorpus(groupped_sentences)
+
+
 if __name__ == '__main__':
     args = sys.argv[1:]
     if '-t' in args:
         train(*args[args.index('-t') + 1:])
     elif '-c' in args:
         classify(*args[args.index('-c') + 1:])
+    elif '-crossv' in args:
+        makeCrossValidation(*args[args.index('-crossv') + 1:])
     else:
         print """
             Usage:
